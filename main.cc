ea231cd09a2abd0da98ef8d54949dda2df00935e
pandaking <aditya9660sharma@gmail.com>
2022-02-14 22:41:14 +0530

    classes added and testing :q

--- main.cc
+++ main.cc
@@ -9,106 +9,121 @@ const std::string HEAD = "head";
 const std::string EDUCATOR = "educator";
 const std::string STUDENT = "student";
 
-void listUsers(std::string msg = "Available users: ") {
-    const std::string red("\033[0;31m");
-    const std::string reset("\033[0m");
-    std::cout << red << std::endl;
-    std::cout << msg << std::endl;
-    // lists users having UIDs from 1000 to 1999 only since its user added
-    system("cat /etc/passwd | grep -E \"1[0-9]{3}\"");
+class DisplayInfo {
+   public:
+    DisplayInfo() {}
 
-    std::cout << reset << std::endl;
-}
+    void listUsers(std::string msg = "Available users: ") {
+        const std::string red("\033[0;31m");
+        const std::string reset("\033[0m");
+        std::cout << red << std::endl;
+        std::cout << msg << std::endl;
+        // lists users having UIDs from 1000 to 1999 only since its user added
+        system("cat /etc/passwd | grep -E \"1[0-9]{3}\"");
 
-/**
- * @brief used for taking in command's output to pipe through popen() makeing
- * use of IPC. command is used for coomand to excute and get back output to
- * user.
- *
- * @param command
- * @return string
- *
- */
-std::string get_popen(const char *command) {
-    FILE *pf;
-    char data[BUFFSIZE];
-    std::string result;
-
-    // Setup our pipe for reading and execute our command.
-    pf = popen(command, "r");
-
-    // Get the data from the process execution
-    fgets(data, BUFFSIZE, pf);
-
-    // the data is now in 'data'
-
-    // Error handling
-    if (pclose(pf) != 0)
-        fprintf(stderr, " Error: Failed to close command stream \n");
-
-    result = data;
-    return result;
-}
-
-/**
- * @brief used for passing commands to pipe through popen() makeing use of IPC.
- *          command is used for commands to be executed and output passed.
- *
- * @param command
- * @param data
- *
- */
-
-void set_popen(const char *command, const char *data) {
-    FILE *pf;
-    // char data[BUFFSIZE];
-
-    // Setup our pipe for writing to file according to our command.
-    pf = popen(command, "w");
-   //std::cout<<data<<std::endl;
-    // Set the data from the process execution
-    fputs(data, pf);
-
-    // the data is now written to file
-
-    // Error handling
-    if (pclose(pf) != 0)
-        fprintf(stderr, " Error: Failed to close command stream \n");
-
-    return;
-}
-
-void add_user(std::string name) {
-    std::string adduser = "sudo adduser ";
-    std::string cmnd = adduser + name;
-    system(cmnd.c_str());  // i.e system("sudo adduser name")
-    // system("sudo useradd -m admin_os"); system(""sudo passwd admin_os");
-    std::cout << "=== Create done ===" << std::endl;
-    listUsers();
-}
-
-void delete_user(std::string name) {
-    // cleanup and deleting of users
-    char y = 'y';
-    std::cout << "Delete user " << name <<" ?[Y/n]" << std::endl;
-    y = getchar();
-    if (y == 'y' || y=='Y' || y == '\n') {
-        std::string cmnd;
-
-        std::cout << "Deleting..." << name << std::endl;
-        //std::cout<<name.size()<<std::endl;   
-        std::string userdel = "sudo userdel -r ";
-        //cmnd = userdel + name + " >/dev/null 2>&1";
-        std::string rmuser = "sudo rmuser -y "+name;
-        system(rmuser.c_str());  // system("sudo rmuser -y  name");
-        std::cout << "=== Delete done ===" << std::endl;
+        std::cout << reset << std::endl;
     }
-}
+};
+
+class Utilities {
+   public:
+    /**
+     * @brief used for taking in command's output to pipe through popen()
+     * makeing use of IPC. command is used for coomand to excute and get back
+     * output to user.
+     *
+     * @param command
+     * @return string
+     *
+     */
+    std::string get_popen(const char *command) {
+        FILE *pf;
+        char data[BUFFSIZE];
+        std::string result;
+
+        // Setup our pipe for reading and execute our command.
+        pf = popen(command, "r");
+
+        // Get the data from the process execution
+        fgets(data, BUFFSIZE, pf);
+
+        // the data is now in 'data'
+
+        // Error handling
+        if (pclose(pf) != 0)
+            fprintf(stderr, " Error: Failed to close command stream \n");
+
+        result = data;
+        return result;
+    }
+
+    /**
+     * @brief used for passing commands to pipe through popen() makeing use of
+     * IPC. command is used for commands to be executed and output passed.
+     *
+     * @param command
+     * @param data
+     *
+     */
+    void set_popen(const char *command, const char *data) {
+        FILE *pf;
+        // char data[BUFFSIZE];
+
+        // Setup our pipe for writing to file according to our command.
+        pf = popen(command, "w");
+        // std::cout<<data<<std::endl;
+        // Set the data from the process execution
+        fputs(data, pf);
+
+        // the data is now written to file
+
+        // Error handling
+        if (pclose(pf) != 0)
+            fprintf(stderr, " Error: Failed to close command stream \n");
+
+        return;
+    }
+};
+
+class User {
+   public:
+    void add_user(std::string name, DisplayInfo display_info) {
+        std::string adduser = "sudo adduser ";
+        std::string cmnd = adduser + name;
+        system(cmnd.c_str());  // i.e system("sudo adduser name")
+        // system("sudo useradd -m admin_os"); system(""sudo passwd admin_os");
+        std::cout << "=== Create done ===" << std::endl;
+        display_info.listUsers();
+    }
+
+    void delete_user(std::string name) {
+        // cleanup and deleting of users
+        char y = 'y';
+        std::cout << "Delete user " << name << " ?[Y/n]" << std::endl;
+        y = getchar();
+        if (y == 'y' || y == 'Y' || y == '\n') {
+            std::string cmnd;
+
+            std::cout << "Deleting..." << name << std::endl;
+            // std::cout<<name.size()<<std::endl;
+            std::string userdel = "sudo userdel -r ";
+            // cmnd = userdel + name + " >/dev/null 2>&1";
+            std::string rmuser = "sudo rmuser -y " + name;
+            system(rmuser.c_str());  // system("sudo rmuser -y  name");
+            std::cout << "=== Delete done ===" << std::endl;
+        }
+    }
+};
 
 int main(int argc, char *argv[]) {
-    listUsers();
+    DisplayInfo display_info;
+    Utilities utilities;
+    User our_user;  // TODO:Have to check the user, how it will work as we
+                    // already have "user"
 
-    std::string user = get_popen("id -u -n");
+    display_info.listUsers();
+
+    std::string user = utilities.get_popen("id -u -n");
     user.erase(user.end() - 1);
     std::cout << "Current user:  " << user << "\n------------------------\n"
               << std::endl;
@@ -123,27 +138,27 @@ int main(int argc, char *argv[]) {
     if (user == ADMIN) {
         std::cout << "I am currently " << user << "!" << std::endl;
         std::cout << "Please Enter name X" << std::endl;
-        getline(std::cin,X);
+        getline(std::cin, X);
 
         std::cout << "Please Enter name Y" << std::endl;
-       getline(std::cin,Y);
+        getline(std::cin, Y);
 
-       std::cout << "Please Enter name Z" << std::endl;
-        getline(std::cin,Z);
+        std::cout << "Please Enter name Z" << std::endl;
+        getline(std::cin, Z);
 
         std::string filename = "file.txt";
         cmnd = "echo \"" + X + ";" + Y + ";" + Z + "\"  > " + filename;
-        //std::string output=system(cmnd.c_str());
-        set_popen(cmnd.c_str(),cmnd.c_str());
+        // std::string output=system(cmnd.c_str());
+        utilities.set_popen(cmnd.c_str(), cmnd.c_str());
 
-        //std::string groupadd = "sudo usermod -a -G sudo admin";
-        //cmnd = groupadd;
-        //system(cmnd.c_str());
-        add_user(HEAD);
-        add_user(STUDENT);
-        add_user(EDUCATOR);
+        // std::string groupadd = "sudo usermod -a -G sudo admin";
+        // cmnd = groupadd;
+        // system(cmnd.c_str());
+        our_user.add_user(HEAD, display_info);
+        our_user.add_user(STUDENT, display_info);
+        our_user.add_user(EDUCATOR, display_info);
     } else if (user == HEAD) {
-        std::string filedata = get_popen("cat file.txt");
+        std::string filedata = utilities.get_popen("cat file.txt");
 
         X = "";
         Y = "";
@@ -168,7 +183,7 @@ int main(int argc, char *argv[]) {
         std::cout << "I am currently " << user << "! Please access: " << X
                   << " " << Y << " " << Z << std::endl;
     } else if (user == EDUCATOR) {
-        std::string filedata = get_popen("cat file.txt");
+        std::string filedata = utilities.get_popen("cat file.txt");
         Y = "";
 
         int cnt = 0;
@@ -185,7 +200,7 @@ int main(int argc, char *argv[]) {
         std::cout << "I am currently " << user << "! Please access: " << Y
                   << std::endl;
     } else if (user == STUDENT) {
-        std::string filedata = get_popen("cat file.txt");
+        std::string filedata = utilities.get_popen("cat file.txt");
 
         X = "";
 
@@ -197,11 +212,9 @@ int main(int argc, char *argv[]) {
         }
         std::cout << "I am currently " << user << "! Please access: " << X
                   << std::endl;
-    }
-
-    else {
+    } else {
         // the starting user (eg:amit) that initiates the program and adds admin
-        add_user(name);
+        our_user.add_user(name, display_info);
     }
 
     std::string groupadd = "sudo groupadd groupname";
@@ -209,10 +222,8 @@ int main(int argc, char *argv[]) {
     // std::string su = "sudo su ";
     // cmnd = su + name;
     // system(cmnd.c_str());
-    
 
-    std::cout<<"\n-------\n";
-    
-    delete_user(name);
-    
+    std::cout << "\n-------\n";
+
+    our_user.delete_user(name);
 }
