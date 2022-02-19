546fc2b4bc4cb5b9f18e25273d7f755e2251e295
emanisowmya <cs19b045@iittp.ac.in>
2022-02-20 05:01:29 +0530

    Permissions added , pw used for add user , files added

--- main.cc
+++ main.cc
@@ -2,12 +2,15 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+using namespace std;
 
 const int BUFFSIZE = 512;
 const std::string ADMIN = "admin";
 const std::string HEAD = "head";
 const std::string EDUCATOR = "educator";
 const std::string STUDENT = "student";
+
+void setPermission(string permission,string filename,string group);
 
 class DisplayInfo {
    public:
@@ -88,10 +91,22 @@ class Utilities {
 class User {
    public:
     void add_user(std::string name, DisplayInfo display_info) {
-        std::string adduser = "sudo adduser ";
-        std::string cmnd = adduser + name;
-        system(cmnd.c_str());  // i.e system("sudo adduser name")
-        // system("sudo useradd -m admin_os"); system(""sudo passwd admin_os");
+
+       /*
+		std::string adduser = "sudo adduser ";
+		std::string cmnd = adduser + name;
+		system(cmnd.c_str());  // i.e system("sudo adduser name")
+		// system("sudo useradd -m admin_os"); system(""sudo passwd admin_os");
+	*/
+                string username;
+                cout<<"Enter the username: ";
+		getline(cin,username);
+		std::string adduser = "sudo pw user add ";
+		std::string cmnd = adduser + username;
+		system(cmnd.c_str());  // i.e system("sudo pw user add name")
+		cout<<"Create a password"<<endl;
+                std::string password = "sudo passwd "+username;
+		system(password.c_str());  // i.e system("sudo pw user add name")
         std::cout << "=== Create done ===" << std::endl;
         display_info.listUsers();
     }
@@ -114,6 +129,65 @@ class User {
         }
     }
 };
+
+void addGroup(string types)
+{
+	string cmnd;
+        cmnd = "pw groupadd "+types;
+        system(cmnd.c_str());
+	setPermission("","xdata","everyone@");
+        setPermission("","xdata","group@");
+	setPermission("","ydata","everyone@");
+        setPermission("","ydata","group@");
+	setPermission("","zdata","everyone@");
+        setPermission("","zdata","group@");
+        
+	if(types == "students")
+	{
+	        setPermission("rca","xdata","g:"+types);
+		setPermission("","ydata","g:"+types);
+		setPermission("","zdata","g:"+types);
+  	}
+        
+	if(types == "heads")
+	{
+	        setPermission("rca","xdata","g:"+types);
+		setPermission("rca","ydata","g:"+types);
+		setPermission("rca","zdata","g:"+types);
+  	}
+        
+	if(types == "educators")
+	{
+	        setPermission("","xdata","g:"+types);
+		setPermission("rca","ydata","g:"+types);
+		setPermission("","zdata","g:"+types);
+  	}
+        
+	if(types == "admins")
+	{
+	        setPermission("rwpaRc","xdata","g:"+types);
+		setPermission("rwpaRc","ydata","g:"+types);
+		setPermission("rwpaRc","zdata","g:"+types);
+  	}
+
+} 
+
+void setPermission(string permission,string filename,string group)
+{
+	string cmnd;
+        cmnd = "setfacl -m "+group+":"+permission+"::allow "+filename;
+        //cout<<cmnd<<endl;
+        system(cmnd.c_str());
+}
+
+void toGroup(string group,string user)
+{
+	string cmnd;
+	cmnd = "sudo pw groupmod "+group+" -m "+user;
+	system(cmnd.c_str());
+
+}
+
 
 int main(int argc, char *argv[]) {
     DisplayInfo display_info;
@@ -138,57 +212,106 @@ int main(int argc, char *argv[]) {
     if (user == ADMIN) {
         std::cout << "Hello Admin\n";
         std::cout << "You have the permission to:\n";
-        std::cout << "\tView and Edit data in File X, File Y, File Z.\n";
-        std::cout << "\tAdd students, educators and heads in their respective groups.\n";
+        std::cout << "\tView and Edit data in File X, File Y, File Z.Enter 1 to do it.\n";
+        std::cout << "\tAdd students, educators and heads in their respective groups.Enter 2 for it\n";
         std::cout << "\tRestrict and modify permissions of any file or users.\n\n";
+     
+        cout<<"Enter which operation you want to do:  ";
+       char y = 'y';
+       y=getchar();
+        if(y=='1')
+     	{
+		cin.ignore();
 
-        std::cout << "Please Enter X" << std::endl;
-        getline(std::cin, X);
+		std::cout << "Please Enter X" << std::endl;
+		getline(std::cin, X);
 
-        std::cout << "Please Enter Y" << std::endl;
-        getline(std::cin, Y);
+		std::cout << "Please Enter Y" << std::endl;
+		getline(std::cin, Y);
 
-        std::cout << "Please Enter Z" << std::endl;
-        getline(std::cin, Z);
+		std::cout << "Please Enter Z" << std::endl;
+		getline(std::cin, Z);
 
-        std::string filename = "file.txt";
-        cmnd = "echo \"" + X + ";" + Y + ";" + Z + "\"  > " + filename;
-        // std::string output=system(cmnd.c_str());
-        utilities.set_popen(cmnd.c_str(), cmnd.c_str());
+		cmnd = "echo \"" + X  + "\"  > " + "xdata";
+		utilities.set_popen(cmnd.c_str(), cmnd.c_str());
+		cmnd = "echo \"" + Y  + "\"  > " + "ydata";
+		utilities.set_popen(cmnd.c_str(), cmnd.c_str());
+		cmnd = "echo \"" + Z  + "\"  > " + "zdata";
+		utilities.set_popen(cmnd.c_str(), cmnd.c_str());
+    	}
 
-        // std::string groupadd = "sudo usermod -a -G sudo admin";
-        // cmnd = groupadd;
-        // system(cmnd.c_str());
-        our_user.add_user(HEAD, display_info);
-        our_user.add_user(STUDENT, display_info);
-        our_user.add_user(EDUCATOR, display_info);
-    } else if (user == HEAD) {
+	else if(y=='2')
+	{
+		// std::string groupadd = "sudo usermod -a -G sudo admin";
+		// cmnd = groupadd;
+		// system(cmnd.c_str());
+		bool flag = true;
+           		
+		while(flag)
+		{
+		        std::cout << "\nEnter 1 if you want to add student\n";
+	       		std::cout << "Enter 2 if you want to add head\n";
+	       		std::cout << "Enter 3 if you want to add educator\n";
+			
+			cout<<"Enter which operation you want to do:  \n";
+			cin.ignore();
+			string type ;
+			getline(cin,type);
+			if(type=="1")
+			{
+				our_user.add_user(STUDENT, display_info);
+				toGroup("wheel","student");
+	       			 toGroup("students","student");
+			}
+			else if(type=="2")
+			{
+				our_user.add_user(HEAD, display_info);
+				toGroup("wheel","head");
+	       			 toGroup("heads","head");
+			}
+			else if(type=="3")
+			{
+				our_user.add_user(EDUCATOR, display_info);
+				toGroup("wheel","educator");
+	       			 toGroup("educators","educator");
+			}
+
+		        std::cout << "\nEnter 1 if you want to add another user\n";
+	       		std::cout << "Enter 2 if you want to exit\n";		
+			char again = 'y';
+			again = getchar();
+			if(again=='2')
+				flag=false;
+		}
+	}
+    } 
+    else if (user == HEAD) {
         std::cout << "Hello Head\n";
         std::cout << "You have the permission to:\n";
         std::cout << "\tView the data in File X, File Y, File Z.\n";
 
         std::string filedata = utilities.get_popen("cat file.txt");
 
-        X = "";
-        Y = "";
-        Z = "";
+        X = utilities.get_popen("cat xdata.txt");
+        Y = utilities.get_popen("cat ydata.txt");
+        Z = utilities.get_popen("cat zdata.txt");
+        /*
+		int cnt = 0;
 
-        int cnt = 0;
-
-        for (int i = 0; i < filedata.size(); i++) {
-            if (filedata[i] == ';') {
-                cnt++;
-                continue;
-            }
-            if (cnt == 0) {
-                X += filedata[i];
-            } else if (cnt == 1) {
-                Y += filedata[i];
-            } else {
-                Z += filedata[i];
-            }
-        }
-
+		for (int i = 0; i < filedata.size(); i++) {
+		    if (filedata[i] == ';') {
+		        cnt++;
+		        continue;
+		    }
+		    if (cnt == 0) {
+		        X += filedata[i];
+		    } else if (cnt == 1) {
+		        Y += filedata[i];
+		    } else {
+		        Z += filedata[i];
+		    }
+		}
+	*/
         std::cout << "Data in File X: " << X << std::endl;
         std::cout << "Data in File Y: " << Y << std::endl;
         std::cout << "Data in File Z: " << Z << std::endl;
@@ -198,20 +321,8 @@ int main(int argc, char *argv[]) {
         std::cout << "You have the permission to:\n";
         std::cout << "\tView the data in File Y.\n";
 
-        std::string filedata = utilities.get_popen("cat file.txt");
-        Y = "";
+        Y = utilities.get_popen("cat ydata.txt");
 
-        int cnt = 0;
-
-        for (int i = 0; i < filedata.size(); i++) {
-            if (filedata[i] == ';') {
-                cnt++;
-                continue;
-            }
-            if (cnt == 1) {
-                Y += filedata[i];
-            }
-        }
         std::cout << "Data in File Y: " << Y << std::endl;
     } else if (user == STUDENT) {
 
@@ -219,30 +330,25 @@ int main(int argc, char *argv[]) {
         std::cout << "You have the permission to:\n";
         std::cout << "\tView the data in File X.\n";
 
-        std::string filedata = utilities.get_popen("cat file.txt");
 
-        X = "";
-
-        for (int i = 0; i < filedata.size(); i++) {
-            if (filedata[i] == ';') {
-                break;
-            }
-            X += filedata[i];
-        }
+        X =  utilities.get_popen("cat xdata.txt");
 
         std::cout << "Data in File X: " << X << std::endl;
     } else {
         // the starting user (eg:amit) that initiates the program and adds admin
+        addGroup("students");
+        addGroup("admins");
+        addGroup("educators");
+        addGroup("heads");
+
+        cout<<endl<<"If admin not found please add the admin."<<endl;
         our_user.add_user(name, display_info);
+        toGroup("wheel","admin");
+        toGroup("admins","admin");
     }
 
-    std::string groupadd = "sudo groupadd groupname";
-    std::string groupdel = "sudo groupdel groupname";
-    // std::string su = "sudo su ";
-    // cmnd = su + name;
-    // system(cmnd.c_str());
 
     std::cout << "\n---------------------------\n";
 
-    our_user.delete_user(name);
+//    our_user.delete_user(name);
 }
